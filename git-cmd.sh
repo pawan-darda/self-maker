4dd207dffe73d7fb45aaa2c44e48cf621d6459ec
Pawan Darda <pawan.darda@gmail.com>
2015-12-26 11:57:50 +0800

    adding more options

--- git-cmd.sh
+++ git-cmd.sh
@@ -1,11 +1,18 @@
 
 cd $1
 
-git add $2
+if [ $# -eq 3 ]
+then
+    git add $2
+    sleep 0.5
+    git commit -a -m "$3"
+    sleep 0.5
+else
+    git commit -a -m "$2"
+    sleep 0.5
+fi
+
+git pull origin master
 sleep 0.5
-git commit -a -m "$3"
-#sleep 0.5
-#git pull origin master
-#sleep 0.5
-#git push origin master
+git push origin master
 

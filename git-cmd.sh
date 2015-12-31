87caf4f36091bb5f29d0053e59a502bf743da97c
Pawan Darda <pawan.darda@gmail.com>
2015-12-31 16:07:18 +0800

    adding messages

--- git-cmd.sh
+++ git-cmd.sh
@@ -4,8 +4,11 @@ cd $1
 
 if [ $# -eq 3 ]
 then
+    echo "GIT Add File"
+    echo $2
     git add $2
     sleep 0.5
+    echo "GIT Commit"
     git commit -a -m "$3"
     sleep 0.5
 else
@@ -13,7 +16,9 @@ else
     sleep 0.5
 fi
 
+echo "GIT Pull"
 git pull origin master
 sleep 0.5
+echo "GIT Push"
 git push origin master
 
--- git_bot.py
+++ git_bot.py
@@ -6,7 +6,6 @@ from git import Repo
 from os import listdir
 from os.path import isfile, join
 
-
 GITCMD = '/Users/pawan/src/self-maker/git-cmd.sh'
 SOURCE = '/Users/pawan/src1/'
 DEST = '/Users/pawan/src/'
@@ -14,7 +13,6 @@ DEST = '/Users/pawan/src/'
 def init_repos():
 
     repo_list = []
-
     repo_names = []
 
     for name in repo_names:
@@ -34,45 +32,57 @@ def copy_random_files():
     count = random.randint(3,10)
 
     print count_files
+
     for iter in range(count):
 
         src_file_path = onlyfiles[random.randint(1,count_files)]
         dest_file_path = src_file_path.replace(SOURCE,DEST)
-        print src_file_path
-        print dest_file_path
+
+        print "Processing File "
+        print "%s" %(dest_file_path)
+        if not (src_file_path or dest_file_path):
+            continue
+
         relative_dest_path = dest_file_path.split(DEST)[1]
         file_items = relative_dest_path.split(os.path.sep)
         repo_name = file_items[0]
         file_name = file_items[-1]
-        repo_path = SOURCE + repo_name
-        print repo_path
+        repo_path = DEST + repo_name + os.path.sep
 
-        if os.path.isfile (dest_file_path):
-            print "Skipping"
-            continue
-
-        message = ''
+        message = "Pushing new files"
         if file_name.endswith("js"):
-            message = "Adding %s javascript file to %s repo" %(file_name.replace(".js",""), repo_name)
+            message = "Adding '%s' javascript file to %s repository" %(file_name.replace(".js",""), repo_name)
         elif file_name.endswith("css"):
-            message = "UI Changes to repo %s" %(repo_name)
+            message = "UI Changes to repo:%s" %(repo_name)
         elif file_name.endswith("py"):
-            message = "Python module %s added to repo %s" %(file_name.replace(".py",""), repo_name)
+            message = "Python module '%s' added to repo %s" %(file_name.replace(".py",""), repo_name)
+        elif file_name.endswith("pyc"):
+            message = "Python Compiled Object : %s" %(file_name.replace(".pyc",""))
         elif file_name.endswith("html"):
-            message = "Template for %s in repo" %(file_name.replace(".html","s"), repo_name)
+            message = "Template for %s in %s" %(file_name.replace(".html","s"), repo_name)
+        elif file_name.endswith("png") or file_name.endswith("jpg") or file_name.endswith("jpeg") :
+            message = "Pushing New Assests"
 
-        print message
-        #shutil.copy (filename1, filename2)
-        #subprocess.call(["sh", GITCMD, ".", message])
+        if not os.path.exists(os.path.dirname(dest_file_path)):
+            os.makedirs(os.path.dirname(dest_file_path))
 
-    subprocess.call(["sh", GITCMD, ".", "Adding random generator"])
+        if not os.path.isfile(dest_file_path):
+            if os.path.isfile(src_file_path):
+                shutil.copy(src_file_path, dest_file_path)
+
+        if os.path.isfile(dest_file_path):
+            subprocess.call(["touch", dest_file_path])
+            if os.path.isfile(src_file_path):
+                os.remove(src_file_path)
+
+        subprocess.call(["sh", GITCMD, repo_path, dest_file_path, message])
+
+        print "Done \n"
 
 def automate():
     copy_random_files()
-    pass
-
+    return
 
 if __name__ == "__main__":
 
-    #init_repos()
     automate()
